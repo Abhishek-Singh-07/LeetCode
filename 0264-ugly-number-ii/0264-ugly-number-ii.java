05a1b290992e81207ed5b0ab306bc57acbd8e251
Abhishek Singh <2k23.csai2313103@gmail.com>
2026-09-18 12:02:49 +0530

    Time: 2 ms (99.8%), Space: 43.2 MB (85.19%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.java
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -1,25 +1,21 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        PriorityQueue<Long> pq = new PriorityQueue<>();
-        HashSet<Long> set = new HashSet<>();
-        pq.add(1L);
-        set.add(1L);
-        long ugly = 1;
-        for (int i = 0; i < n; i++) {
-            ugly = pq.poll();
-            long num2 = ugly * 2;
-            long num3 = ugly * 3;
-            long num5 = ugly * 5;
-            if (set.add(num2)) {
-                pq.add(num2);
+        int[] dp=new int[n];
+        dp[0]=1;
+        int i2=0,i3=0,i5=0;
+        for(int i=1;i<n;i++){
+            int next2=dp[i2]*2,next3=dp[i3]*3,next5=dp[i5]*5;
+            dp[i]=Math.min(next2,Math.min(next3,next5));
+            if(dp[i]==next2){
+                i2++;
             }
-            if (set.add(num3)) {
-                pq.add(num3);
+            if(dp[i]==next3){
+                i3++;
             }
-            if (set.add(num5)) {
-                pq.add(num5);
+            if(dp[i]==next5){
+                i5++;
             }
         }
-        return (int) ugly;
+        return dp[n-1];
     }
 }
